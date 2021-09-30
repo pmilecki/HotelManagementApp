8deed15771afde2daa8439cd31af2798d8d3bf64
Piotr Milecki <piotr.milecki@brandbq.pl>
2021-10-01 00:41:45 +0200

    Add project files.

---
+++ HotelManagementApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HotelManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
